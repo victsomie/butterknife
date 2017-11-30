72377d9944d262d4589617325b55ca880914c1cd
victsomie <victsomie@yahoo.com>
2017-11-30 10:47:37 +0300

    Stuff done using butterknife

--- app/src/main/java/com/example/admin/butterknife/MainActivity.java
+++ app/src/main/java/com/example/admin/butterknife/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.admin.butterknife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Binding views
+    @BindView(R.id.tv_title)
+    TextView title;
+
+    @BindView(R.id.et_input)
+    EditText input;
+
+    @BindView(R.id.btn_submit)
+    Button update;
+
+    // Binding a method
+    @OnClick(R.id.btn_submit) void update(){
+
+        // Do somethinge here
+        if (!input.getText().toString().isEmpty()){
+            title.setText(input.getText().toString());
+        }
+        else {
+            Toast.makeText(this, "Please enter some text", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // You won't cut anything if you wont pick the knife like below
+        ButterKnife.bind(this);
+
+
+        //update();
+
     }
 }
